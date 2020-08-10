7799efdef5c007422679f677c02399a1917ff240
Damian Scorciapino <damian_scorciapino@hotmail.com>
2020-08-10 16:11:24 -0300

    Docs

---
+++ docs/precache-manifest.3429e82f23190f3dd9cd7ccf58d7027c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "474b49eef593228dcd7257c446cdf60c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e69539a082fe5fb93b8d",
+    "url": "/static/css/main.2614efe1.chunk.css"
+  },
+  {
+    "revision": "66edcb195c6f47856818",
+    "url": "/static/js/2.8c7c1195.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8c7c1195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e69539a082fe5fb93b8d",
+    "url": "/static/js/main.1e741d48.chunk.js"
+  },
+  {
+    "revision": "28b2448ea3201b2f979f",
+    "url": "/static/js/runtime-main.09460a5c.js"
+  }
+]);
